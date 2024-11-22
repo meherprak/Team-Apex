77967a7c7b3bdc8b26e634c8f23ec84f2bfe7d8a
meherprak <131869657+meherprak@users.noreply.github.com>
2024-11-22 22:35:42 +0530

    Day -1
    started learning typescript
    understood static types, uses of Ts,started reading typescript documentation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Team-Apex
+
+22/11/24 started learning type script
---
+++ Typescript/index.ts
@@ -0,0 +1,14 @@
+console.log("meher");
+//declaring string variable
+let greetings:string="hello world"
+greetings.toUpperCase();
+console.log(greetings);
+
+
+// declaring a number variable
+let n:number=5689;
+
+
+//boolean
+let logged:boolean=false;
+
