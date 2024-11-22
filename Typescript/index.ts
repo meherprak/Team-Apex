d51245e5c7fd19fd5b5eb5556c2de9b146ff10c2
meherprak <131869657+meherprak@users.noreply.github.com>
2024-11-22 22:41:11 +0530

    Day 1 continuation

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -12,3 +12,8 @@ let n:number=5689;
 //boolean
 let logged:boolean=false;
 
+
+// understood where to use static variable declaration and where not to and also how typescript automatically assigns datatype to variable 
+
+//understood also "any" keyword and why it should be used sparingly
+
